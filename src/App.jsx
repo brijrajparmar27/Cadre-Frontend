258eac095e511aabaf19317e327a5151700b5fcc
brijraj <brijrajparmar27@gmail.com>
2023-02-17 10:35:16 +0530

    implemented Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Auth from "./Pages/Auth/Auth";
+import Dashboard from "./Pages/Dashboard/Dashboard";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/Auth/Auth.jsx
@@ -0,0 +1,5 @@
+const Auth = () => {
+  return <div className="auth">auth</div>;
+};
+
+export default Auth;
