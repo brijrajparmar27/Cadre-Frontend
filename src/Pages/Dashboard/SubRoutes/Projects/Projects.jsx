4de3a3a22271b60a8adb4cacb5d0bb31303c93cb
brijraj <brijrajparmar27@gmail.com>
2023-02-22 16:21:01 +0530

    created projects header UI

--- src/Pages/Dashboard/SubRoutes/Projects/Projects.jsx
+++ src/Pages/Dashboard/SubRoutes/Projects/Projects.jsx
@@ -1,22 +1,51 @@
 import React from "react";
-import { Navigate, useNavigate } from "react-router";
-import ToggleButton from "react-toggle-button";
+import { useNavigate } from "react-router";
+import { BsSearch, BsFilter, BsPlusLg } from "react-icons/bs";
 import "./Projects.css";
+import Select from "react-select";
 
 export default function Projects() {
-  const navigate=useNavigate();
+  const navigate = useNavigate();
+  const sortingOptions = [
+    { value: "created", label: "Start Time" },
+    { value: "deadline", label: "Deadline" },
+    { value: "tasks", label: "Tasks" },
+    { value: "progress", label: "Progress" },
+  ];
   return (
     <div className="projects">
-      <h1>Dashboard</h1>
+      <div className="section_title">
+        <h1>Dashboard</h1>
+
+        <button
+          className="create_project btn"
+          onClick={() => {
+            navigate("/dashboard/addProject");
+          }}
+        >
+          <BsPlusLg className="add_btn" />
+          New Project
+        </button>
+      </div>
       <div className="header_bar">
-        <input type="text" />
-        <select name="sort" id="sort">
-          <option value="created">Start Time</option>
-          <option value="deadline">Deadline</option>
-          <option value="tasks">Tasks</option>
-          <option value="progress">Progress</option>
-        </select>
-        <button  onClick={()=>{navigate('/dashboard/addProject')}}>create project</button>
+        <div className="search_contain">
+          <BsSearch className="search_icon" />
+          <input
+            type="text"
+            className="proj_search_tb"
+            placeholder="Search Project"
+          />
+        </div>
+        <div className="sortby">
+          <BsFilter className="filter_icon" />
+          <div style={{ width: "100%" }}>
+            <Select
+              options={sortingOptions}
+              isSearchable={false}
+              placeholder="Sort By"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
