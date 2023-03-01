a91619c724ed891021fbc9a3ce90f58944585eb4
brijraj <brijrajparmar27@gmail.com>
2023-03-01 12:09:30 +0530

    cleanup

--- src/Pages/Dashboard/SubRoutes/Project/Project.jsx
+++ src/Pages/Dashboard/SubRoutes/Project/Project.jsx
@@ -1,6 +1,5 @@
 import "./Project.css";
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import {
   MdOutlineKeyboardArrowUp,
