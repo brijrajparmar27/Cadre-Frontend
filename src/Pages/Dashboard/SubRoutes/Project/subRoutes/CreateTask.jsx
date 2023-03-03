7e7fb0113c35974d267ec4025cd61e371fa70438
bhautik <bhautik.zanzrukiya@coodeit.com>
2023-03-03 10:36:51 +0530

    -show error message in create task

--- src/Pages/Dashboard/SubRoutes/Project/subRoutes/CreateTask.jsx
+++ src/Pages/Dashboard/SubRoutes/Project/subRoutes/CreateTask.jsx
@@ -7,11 +7,11 @@ function CreateTask() {
   const location = useLocation();
   const navigate = useNavigate();
   const [membes, setMembers] = useState([]);
+  const [error, setError] = useState();
   const { addTask } = useProject();
   let options = location?.state?.member.map(function (data) {
     return { value: data, label: data.name };
   });
-  console.log(location.state);
   const inputSubmit = (e) => {
     e.preventDefault();
     const obj = {
@@ -22,10 +22,21 @@ function CreateTask() {
       project: location.state._id,
     };
     const data = { prev: [...location?.state?.task], new: obj };
-    addTask(data).then(res=>{
-      console.log(res);
-      navigate("/dashboard/project",{state:{projectdata:res.data}})
-    });
+    console.log(obj.title)
+    if (
+      obj.title.length > 0 &&
+      obj.descripation.length > 0 &&
+      obj.assigned.length > 0
+    ) {
+      addTask(data).then((res) => {
+        console.log(res);
+        navigate("/dashboard/project", { state: { projectdata: res.data } });
+      }); 
+    }
+    else{
+        setError('field cannot be empty')
+    }
+   
   };
   const inputmember = (e) => {
     let membersarry = [];
@@ -40,6 +51,7 @@ function CreateTask() {
         descripation <input name="descripation" type="text" />
         Title <input name="title" type="text" />
         <Select options={options} isMulti onChange={inputmember}></Select>
+        <p className="error_msg">{error}</p>
         <button type="submit">submit</button>
       </form>
     </div>
