a9e6538b125504ebd941f43ec4124729aae3a830
bhautik <bhautik.zanzrukiya@coodeit.com>
2023-03-09 14:51:31 +0530

    -setting componet

--- src/Pages/Dashboard/SubRoutes/Settings/Settings.jsx
+++ src/Pages/Dashboard/SubRoutes/Settings/Settings.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Settings() {
-  return <div>Settings</div>;
+  return (
+    <>
+      <div className="setting">
+        <div className="section_title">
+          <h1>Project Details</h1>
+        </div>
+        <div className="setting_contain">
+           <form>
+                Name<input></input>
+                Phone<input></input>
+           </form>
+        </div>
+      </div>
+    </>
+  );
 }
