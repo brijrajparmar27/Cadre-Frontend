fc481293244408cab6ca40e29c242921da0683f7
brijraj <brijrajparmar27@gmail.com>
2023-03-09 16:48:12 +0530

    DP upload initiated

--- src/Hooks/useUserCollection.jsx
+++ src/Hooks/useUserCollection.jsx
@@ -12,9 +12,19 @@ function useUserCollection() {
         console.log(err);
       });
   };
+  const updateUserDP = (data) => {
+    API.post(`/update-user-dp`, data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return {
     getAlluser,
     userdata,
+    updateUserDP,
   };
 }
 
--- src/Pages/Dashboard/SubRoutes/Settings/Settings.jsx
+++ src/Pages/Dashboard/SubRoutes/Settings/Settings.jsx
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useRef } from "react";
+import "./Settings.css";
+import avatar from "../../../../assets/images/avatar.svg";
+import useUserCollection from "../../../../Hooks/useUserCollection";
+import { useSelector } from "react-redux";
 
 export default function Settings() {
+  const { updateUserDP } = useUserCollection();
+  const { userData } = useSelector((state) => state.logindataslice);
+
+  const inputFileRef = useRef();
+  const onFileChangeCapture = (e) => {
+    // console.log(e.target.files[0]);
+    const formdata = new FormData();
+    formdata.append("Image", e.target.files[0]);
+    formdata.append("Id", userData._id);
+    /*Selected files data can be collected here.*/
+    updateUserDP(formdata);
+  };
+  const onBtnClick = () => {
+    /*Collecting node-element and performing click*/
+    inputFileRef.current.click();
+  };
   return (
     <>
-      <div className="setting">
+      <div className="settings">
         <div className="section_title">
-          <h1>Project Details</h1>
+          <h1>Settings</h1>
         </div>
-        <div className="setting_contain">
-           <form>
-                Name<input></input>
-                Phone<input></input>
-           </form>
+        <div className="settings_contain">
+          <div className="dp_contain">
+            <img src={avatar} alt="" onClick={onBtnClick} />
+            <input
+              name="image"
+              type="file"
+              ref={inputFileRef}
+              onChangeCapture={onFileChangeCapture}
+              style={{ display: "none" }}
+            />
+          </div>
+          <form>
+            <p className="input_label">Name</p>
+            <input type="text" className="textbox" name="name" />
+            <p className="input_label">Phone Number</p>
+            <input
+              type="number"
+              className="textbox"
+              name="assigenedate"
+              required
+            />
+          </form>
         </div>
       </div>
     </>
