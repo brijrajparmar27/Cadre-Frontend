07eb80b74544376b1a690e5e26501409b2b4926f
brijraj <brijrajparmar27@gmail.com>
2023-03-24 17:26:58 +0530

    update project implemented

--- src/Pages/Dashboard/SubRoutes/EditProject/EditProject.jsx
+++ src/Pages/Dashboard/SubRoutes/EditProject/EditProject.jsx
@@ -5,83 +5,76 @@ import { useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 import Select from "react-select";
 import useProject from "../../../../Hooks/useProject";
-import useSendmail from "../../../../Hooks/useSendmail";
 import useUserCollection from "../../../../Hooks/useUserCollection";
 import "./EditProject.css";
+
 export default function EditProject() {
-  const [selectMebers, setSelectMebers] = useState([]);
-  const [selectStack, setSelectStck] = useState([]);
-  const { getAlluser, userdata } = useUserCollection();
-  const { getAllSatck, stackdata,updateProject } = useProject();
+  const navigate = useNavigate();
+
   const { userData } = useSelector((state) => state.logindataslice);
-  const { sendmail } = useSendmail();
-  const location=useLocation();
-  console.log('location',location)
-  let options = userdata.map(function (data) {
-    return { value: data, label: data.name };
+  const location = useLocation();
+
+  const { getAlluser, userdata } = useUserCollection();
+  const { getAllSatck, stackdata, updateProject } = useProject();
+
+  const [userOptions, setUserOptions] = useState([]);
+  const [stackOptions, setStackOptions] = useState([]);
+
+  const [projectData, setProjectData] = useState({
+    project_name: location.state.project_name,
+    description: location.state.description,
+    assigned_date: location.state.assigned_date,
+    deadline: location.state.deadline,
+    member: location.state.member,
+    progress: location.state.progress,
+    lead: location.state.lead,
+    stack: location.state.stack,
   });
-  let stckOption = stackdata.map((data) => {
-    return { value: data, label: data.title };
-  });
-  let defaultMembers=location.state.member.map((data)=>{
-       return{ value:data,label:data.name}
-  });
-  let DefaultProjectTech=location.state.stack.map((data)=>{
-       return{value:data,label:data.title}
-  })
+
+  const mutateProjectData = (key, value) => {
+    setProjectData((prev) => {
+      let temp = prev;
+      temp[key] = value;
+      return temp;
+    });
+  };
+
+  console.log("location", location);
+
   useEffect(() => {
     getAlluser();
     getAllSatck();
   }, []);
+
   useEffect(() => {
-    console.log(selectMebers);
-  }, [selectMebers]);
-  const populateMembers = (members) => {
-    let memberArr = [];
-    members.forEach((member) => {
-      memberArr.push(member.value);
-      setSelectMebers(memberArr);
-    });
-    memberArr = [];
-  };
-  const inputstck = (e) => {
-    console.log(e);
-    let stackarry = [];
-    e.forEach((stack) => {
-      stackarry.push(stack.value);
-      setSelectStck(stackarry);
-    });
-  };
+    console.log(userdata, stackdata);
+    console.log(location.state.member);
+    console.log(location.state.stack);
+    setStackOptions(
+      stackdata.map((each) => {
+        return { value: each, label: each.title };
+      })
+    );
+    setUserOptions(
+      userdata.map((each) => {
+        return { value: each, label: each.name };
+      })
+    );
+  }, [userdata, stackdata]);
+
   const handelsubmit = (e) => {
     e.preventDefault();
     const currentUser = { ...userData };
     delete currentUser?.jwt;
-    const projectdata = {
-      project_name: e.target.projectname.value.trim(),
-      description: e.target.projectDescripation.value.trim(),
-      assigned_date: e.target.assigenedate.value.trim(),
-      deadline: e.target.DeadLine.value.trim(),
-      member: selectMebers,
-      progress: 0,
-      lead: currentUser,
-      stack: selectStack,
-    };
-    console.log(projectdata);
-    if (
-      projectdata.project_name.length > 0 &&
-      projectdata.description.length > 0 &&
-      projectdata.assigned_date.length > 0 &&
-      projectdata.deadline.length > 0 &&
-      projectdata.member.length > 0
-    ) {
-      updateProject(projectdata,location.state._id);
-
-      e.target.reset();
+    console.log(projectData);
+    if (projectData.member.length > 0 || projectData.stack.length > 0) {
+      updateProject(projectData, location.state._id);
+      // e.target.reset();
     } else {
       console.log("cannot be empty");
     }
   };
-  const navigate = useNavigate();
+
   return (
     <div className="create_project_section">
       <div className="section_title">
@@ -96,23 +89,78 @@ export default function EditProject() {
 
       <form onSubmit={handelsubmit} className="create_content">
         <p className="input_label">Project Name</p>
-        <input type="text" className="textbox" name="projectname" required defaultValue={location.state.project_name} />
+        <input
+          type="text"
+          className="textbox"
+          name="projectname"
+          required
+          defaultValue={projectData.project_name}
+          onChange={(e) => {
+            mutateProjectData("project_name", e.target.value.trim());
+          }}
+        />
         <p className="input_label">Project Description</p>
         <input
           type="text"
           className="textbox"
           name="projectDescripation"
           required
-          defaultValue={location.state.description}
+          defaultValue={projectData.description}
+          onChange={(e) => {
+            mutateProjectData("description", e.target.value.trim());
+          }}
         />
         <p className="input_label">Assigned Date</p>
-        <input type="Date" className="textbox" name="assigenedate" required   defaultValue={moment(location.state.assigned_date).format("YYYY-MM-DD")}/>
+        <input
+          type="Date"
+          className="textbox"
+          name="assigenedate"
+          required
+          defaultValue={moment(projectData.assigned_date).format("YYYY-MM-DD")}
+          onChange={(e) => {
+            mutateProjectData("assigned_date", e.target.value);
+          }}
+        />
         <p className="input_label">Project Deadline</p>
-        <input type="Date" className="textbox" name="DeadLine"  defaultValue={moment(location.state.deadline).format("YYYY-MM-DD")} />
+        <input
+          type="Date"
+          className="textbox"
+          name="DeadLine"
+          defaultValue={moment(projectData.deadline).format("YYYY-MM-DD")}
+          onChange={(e) => {
+            mutateProjectData("deadline", e.target.value);
+          }}
+        />
         <p className="input_label">Select Project Members</p>
-        <Select options={options} isMulti onChange={populateMembers} required   defaultValue={defaultMembers || Select}/>
+        <Select
+          options={userOptions}
+          isMulti
+          required
+          defaultValue={projectData.member.map((each) => {
+            return { value: each, label: each.name };
+          })}
+          onChange={(e) => {
+            mutateProjectData(
+              "member",
+              e.map((each) => each.value)
+            );
+          }}
+        />
         <p className="input_label">Project Technologies</p>
-        <Select options={stckOption} isMulti onChange={inputstck} required defaultValue={DefaultProjectTech} />
+        <Select
+          options={stackOptions}
+          isMulti
+          required
+          defaultValue={projectData.stack.map((each) => {
+            return { value: each, label: each.title };
+          })}
+          onChange={(e) => {
+            mutateProjectData(
+              "stack",
+              e.map((each) => each.value)
+            );
+          }}
+        />
         <button type="submit" className="create_project">
           Update Project
         </button>
