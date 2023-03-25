cd055f1368e8f29989a4cebbd549c5309862587d
brijraj <brijrajparmar27@gmail.com>
2023-03-25 11:58:13 +0530

    added progress provider

---
+++ src/Hooks/useProgressProvider.jsx
@@ -0,0 +1,33 @@
+const useProgresProvider = () => {
+  function calculateProgress(tasks) {
+    let totalTasks, taskStatuses;
+
+    totalTasks = tasks.length();
+    taskStatuses = tasks.map((each) => {
+      return each.status;
+    });
+
+    const progressPoints = {
+      pending: 0,
+      running: 25,
+      completed: 75,
+      closed: 100,
+    };
+
+    let totalProgress = 0;
+    for (let i = 0; i < totalTasks; i++) {
+      const taskStatus = taskStatuses[i];
+      if (progressPoints.hasOwnProperty(taskStatus)) {
+        totalProgress += progressPoints[taskStatus];
+      } else {
+        // invalid status, assume 0 progress
+        console.warn(`Invalid status: ${taskStatus}`);
+      }
+    }
+
+    return totalProgress / totalTasks;
+  }
+  return calculateProgress;
+};
+
+export default useProgresProvider;
