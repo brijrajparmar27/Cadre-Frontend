c94e24b15c8cb07b9d33014af970ab7231a82c07
brijraj <brijrajparmar27@gmail.com>
2023-04-08 23:46:10 +0530

    added chat contacts

--- src/Hooks/useChat.jsx
+++ src/Hooks/useChat.jsx
@@ -1,37 +1,61 @@
-import React from 'react'
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
-import API from '../Pages/axios/axios'
+import React from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import API from "../Pages/axios/axios";
 
 function useChat() {
-    const [selectedchat,setSelectedChat]=useState();
-    const [getChatdata,setGetChat]=useState()
-    const { userData } = useSelector((state) => state.logindataslice);
-    const config = {
-        headers: { Authorization: `Bearer ${userData.jwt}` }
-    };
-    const accessChat=(userId)=>{
-        API.post('/chat',{userId},config).then((res)=>{
-            setSelectedChat(res.data);
-        }).catch((err)=>{
-            console.log(err);
-        })
-    }
-    const groupChat=(users,name)=>{
-        API.post('/Groupchat',{users,name},config).then((res)=>{
-            console.log(res);
-        }).catch((err)=>{
-            console.log(err);
-        })
-    }
-    const FetchChat=()=>{
-        API.get('/chat',config).then((res)=>{
-            setGetChat(res.data)
-        }).catch((err)=>{
-            console.log(err);
-        })
-    }
-  return {accessChat,FetchChat,selectedchat,groupChat,getChatdata,setSelectedChat}
+  const [selectedchat, setSelectedChat] = useState();
+  const [chatData, setChatData] = useState();
+
+  const { userData } = useSelector((state) => state.logindataslice);
+
+  const config = {
+    headers: { Authorization: `Bearer ${userData.jwt}` },
+  };
+
+  const accessChat = (userId) => {
+    API.post("/chat", { userId }, config)
+      .then((res) => {
+        setSelectedChat(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getContacts = (id, query) => {
+    return API.get(`/get-user-and-project/${id}?search=${query}`);
+  };
+
+  const groupChat = (users, name) => {
+    API.post("/Groupchat", { users, name }, config)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const FetchChat = () => {
+    API.get("/chat", config)
+      .then((res) => {
+        setChatData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return {
+    accessChat,
+    FetchChat,
+    selectedchat,
+    groupChat,
+    chatData,
+    getContacts,
+    setSelectedChat,
+  };
 }
 
-export default useChat
+export default useChat;
--- src/Pages/Dashboard/SubRoutes/Chat/Chat.jsx
+++ src/Pages/Dashboard/SubRoutes/Chat/Chat.jsx
@@ -1,185 +1,62 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import useChat from "../../../../Hooks/useChat";
-import useMessage from "../../../../Hooks/useMessage";
-import useUserCollection from "../../../../Hooks/useUserCollection";
 import "./Chat.css";
 import io from "socket.io-client";
-import Select from "react-select";
+import useChat from "../../../../Hooks/useChat";
+import { useSelector } from "react-redux";
 const ENDPOINT = "http://localhost:4040/";
-var socket, selectedchatCompare;
 export default function Chat() {
-  const { getAlluser, userdata} =
-    useUserCollection();
-  const [userChat, setuserChat] = useState();
-  const [socketConnection, setSocketConnection] = useState(false);
-  const [typing, setTyping] = useState(false);
-  const [isTyping, setisTyping] = useState(false);
-  const {
-    accessChat,
-    FetchChat,
-    selectedchat,
-    groupChat,
-    getChatdata,
-    setSelectedChat,
-  } = useChat();
   const { userData } = useSelector((state) => state.logindataslice);
-  const { sendMessage, FetachallMessage, displayMessage, setDisplayMessage } =
-    useMessage();
-
-  const clickuser = (data) => {
-    setuserChat(data);
-    if (data.isGroupChat == false) {
-      try {
-        accessChat(data?.users[1]?._id);
-      } catch (err) {
+  const { getContacts } = useChat();
+  const [contacts, setContacts] = useState([]);
+  const [CurrentChat, setCurrentChat] = useState(null);
+  const [query, setQuery] = useState("");
+  useEffect(() => {
+    console.log(query);
+  }, [query]);
+  useEffect(() => {
+    getContacts(userData._id, query)
+      .then((res) => {
+        setContacts(res.data);
+      })
+      .catch((err) => {
         console.log(err);
-      }
-    } else {
-      setSelectedChat(data);
-    }
-  };
-  const sendingMessage = (e) => {
-    socket.emit("stope typing", selectedchat._id);
-    e.preventDefault();
-    sendMessage({
-      content: e.target.chatBox.value,
-      chatId: selectedchat._id,
-    }).then((res) => {
-      socket.emit("new message", res);
-    });
-  };
-  useEffect(() => {
-    socket = io(ENDPOINT);
-    socket.emit("setup", userData);
-    socket.on("connected", () => setSocketConnection(true));
-    socket.on("typing", () => setisTyping(true));
-    socket.on("stope typing", () => setisTyping(false));
-  }, []);
-  useEffect(() => {
-    FetchChat();
-    getAlluser();
-  }, []);
-  useEffect(() => {
-    console.log("mess");
-    socket.on("message", (newMessageRecive) => {
-      console.log("newmessage", newMessageRecive);
-      console.log(selectedchatCompare);
-      if (
-        !selectedchatCompare ||
-        selectedchatCompare._id === newMessageRecive.chat._id
-      ) {
-        //give notification
-        console.log("fjfj");
-      } else {
-        setDisplayMessage([...displayMessage, newMessageRecive]);
-      }
-    });
-  });
-  useEffect(() => {
-    if (!selectedchat) {
-      return;
-    } else {
-      FetachallMessage(selectedchat?._id);
-      socket.emit("join chat", selectedchat._id);
-      selectedchatCompare = selectedchat;
-    }
-  }, [selectedchat]);
-  const typinghandele = () => {
-    console.log(socketConnection);
-    console.log(typing);
-    if (!socketConnection) return;
-    if (!typing) {
-      console.log("typing");
-      setTyping(true);
-      socket.emit("typing", selectedchat._id);
-    }
-    let lastTypingTime = new Date().getTime();
-    let timerlenght = 3000;
-    setTimeout(() => {
-      var timenow = new Date().getTime();
-      let timediff = timenow - lastTypingTime;
-      if (timediff >= timerlenght && typing) {
-        socket.emit("stope typing", selectedchat._id);
-        setTyping(false);
-      }
-    }, timerlenght);
-  };
-  let options = userdata.map(function (data) {
-    return { value: data, label: data.name };
-  });
-  const inputsearch = (members) => {
-    console.log(members);
-    //setuserChat(members.label)
-    accessChat(members.value._id);
-  };
+      });
+  }, [query]);
   return (
     <div className="chats">
       <div className="chatsidebar">
-        <Select options={options} onChange={inputsearch} required />
-        <input type="text" className="search_box" />
+        <input
+          type="text"
+          className="search_box"
+          onChange={(e) => {
+            setQuery(e.target.value.trim());
+          }}
+        />
         <div className="chat_contacts">
-          {getChatdata?.map((data) => {
-            return (
-              <>
-                {data?.isGroupChat == true ? (
-                  <a
-                    onClick={() => {
-                      clickuser(data);
-                    }}
-                  >
-                    {data.chatName}
-                  </a>
-                ) : (
-                  <a
-                    onClick={() => {
-                      clickuser(data);
-                    }}
-                  >
-                    {data && data.users && data.users[1]?.name}
-                  </a>
-                )}
-              </>
-            );
-          })}
+          {contacts &&
+            contacts.map((each) => {
+              return (
+                <p key={each._id}>
+                  {each.name ? each.name : each.project_name}
+                </p>
+              );
+            })}
         </div>
       </div>
 
       <div className="content">
         <div className="navbar">
-          <a href="#home">
-            {userChat?.isGroupChat == true
-              ? userChat?.chatName
-              : userChat?.users[1]?.name}
-          </a>
+          <a href="#home">home</a>
         </div>
-        <div className="chats_contain">
-          {displayMessage?.map((data) => {
-            return (
-              <>
-                {data?.sender?._id === userData?._id ? (
-                  <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                    <p>{data?.content}</p>
-                  </div>
-                ) : (
-                  <div>
-                    <p>{data?.content}</p>
-                  </div>
-                )}
-              </>
-            );
-          })}
-           {isTyping ? "typing...." : ""}
-        </div>
+        <div className="chats_contain"></div>
 
-        <form onSubmit={sendingMessage} className="chat_form">
+        <form onSubmit={() => {}} className="chat_form">
           <input
             id="text-message"
             type="text"
             name="chatBox"
             className="new_msg_box"
             placeholder="Type your message here ..."
-            onChange={typinghandele}
           />
           <button type="submit" className="new_msg_send">
             Send
