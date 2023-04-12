4156d5f5d98f3a6b6394ebbc6bf0129deaff34e2
brijraj <brijrajparmar27@gmail.com>
2023-04-12 11:54:28 +0530

    fixed text overflow in last text

--- src/Pages/Dashboard/SubRoutes/Chat/ContactCard/ContactCard.jsx
+++ src/Pages/Dashboard/SubRoutes/Chat/ContactCard/ContactCard.jsx
@@ -23,7 +23,10 @@ export default function ContactCard({ each, handleChatOpen }) {
       <div className="contact_name">
         {each.name ? each.name : each.chatName}
       </div>
-      <div className="contact_last_text">{each?.latestMessage?.content}</div>
+      <div className="contact_last_text">
+        {each?.latestMessage?.content.substring(0, 20)}
+        {each?.latestMessage?.content.length > 20 && "..."}
+      </div>
     </div>
   );
 }
